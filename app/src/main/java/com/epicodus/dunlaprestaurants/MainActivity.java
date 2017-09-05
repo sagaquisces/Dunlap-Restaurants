af853b39f47a509f1f3b4eaccf8b5e4ba60bfb18
Michael Dunlap <michaeldunlap@earthlink.net>
2017-09-05 11:58:44 -0700

    app will list restaurants in restaurant activity from static array.

--- app/src/main/java/com/epicodus/dunlaprestaurants/MainActivity.java
+++ app/src/main/java/com/epicodus/dunlaprestaurants/MainActivity.java
@@ -1,22 +1,29 @@
 package com.epicodus.dunlaprestaurants;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindRestaurantsButton;
     private EditText mLocationEditText;
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
         mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        Typeface caviarFont = Typeface.createFromAsset(getAssets(), "fonts/caviar-dreams-regular.ttf");
+        mAppNameTextView.setTypeface(caviarFont);
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {

64ba719df55d1899c0f08137feab27a0b94de8cd
Michael Dunlap <michaeldunlap@earthlink.net>
2017-09-05 15:14:34 -0700

    refactored onClick code

--- app/src/main/java/com/epicodus/dunlaprestaurants/MainActivity.java
+++ app/src/main/java/com/epicodus/dunlaprestaurants/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
     @Bind(R.id.locationEditText) EditText mLocationEditText;
     @Bind(R.id.appNameTextView) TextView mAppNameTextView;
@@ -25,14 +25,16 @@ public class MainActivity extends AppCompatActivity {
         Typeface caviarFont = Typeface.createFromAsset(getAssets(), "fonts/caviar-dreams-regular.ttf");
         mAppNameTextView.setTypeface(caviarFont);
 
-        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String location = mLocationEditText.getText().toString();
-                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
-                intent.putExtra("location", location);
-                startActivity(intent);
-            }
-        });
+        mFindRestaurantsButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mFindRestaurantsButton) {
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
     }
 }

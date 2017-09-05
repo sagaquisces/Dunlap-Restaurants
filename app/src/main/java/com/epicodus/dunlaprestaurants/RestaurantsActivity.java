f1f9a8a8f663295006858f127e56dab43e9801aa
Michael Dunlap <michaeldunlap@earthlink.net>
2017-09-05 10:03:16 -0700

    fill out form for zip code and get string to logcat

---
+++ app/src/main/java/com/epicodus/dunlaprestaurants/RestaurantsActivity.java
@@ -0,0 +1,13 @@
+package com.epicodus.dunlaprestaurants;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestaurantsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurants);
+    }
+}

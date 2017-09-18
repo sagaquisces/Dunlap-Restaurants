648134c58d38c98c13c75ee758f6b50e47c1f072
Michael Dunlap <michaeldunlap@earthlink.net>
2017-09-18 15:57:02 -0700

    login page launches application

---
+++ app/src/main/java/com/epicodus/dunlaprestaurants/ui/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package com.epicodus.dunlaprestaurants.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.epicodus.dunlaprestaurants.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}

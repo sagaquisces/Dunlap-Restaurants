088b85a305c1363c5a614fcd9caf2391edf2554c
Michael Dunlap <michaeldunlap@earthlink.net>
2017-09-25 10:56:52 -0700

    saved list listens to firebase and will remove list item when database node is deleted.

---
+++ app/src/main/java/com/epicodus/dunlaprestaurants/ui/SavedRestaurantListActivity.java
@@ -0,0 +1,56 @@
+package com.epicodus.dunlaprestaurants.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.epicodus.dunlaprestaurants.Constants;
+import com.epicodus.dunlaprestaurants.R;
+import com.epicodus.dunlaprestaurants.adapters.FirebaseRestaurantViewHolder;
+import com.epicodus.dunlaprestaurants.models.Restaurant;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class SavedRestaurantListActivity extends AppCompatActivity {
+    private DatabaseReference mRestaurantReference;
+    private FirebaseRecyclerAdapter mFirebaseAdapter;
+
+    @Bind(R.id.recyclerView)  RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_restaurants);
+        ButterKnife.bind(this);
+
+        mRestaurantReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_RESTAURANTS);
+        setUpFirebaseAdapter();
+    }
+
+    private void setUpFirebaseAdapter() {
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Restaurant, FirebaseRestaurantViewHolder>(Restaurant.class, R.layout.restaurant_list_item, FirebaseRestaurantViewHolder.class, mRestaurantReference) {
+            @Override
+            protected void populateViewHolder(FirebaseRestaurantViewHolder viewHolder, Restaurant model, int position) {
+                viewHolder.bindRestaurant(model);
+            }
+
+        };
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mFirebaseAdapter.cleanup();
+    }
+
+
+}

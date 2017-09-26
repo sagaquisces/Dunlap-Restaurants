2b1d1716f688891bf76f48a16461e543eb04028e
Michael Dunlap <michaeldunlap@earthlink.net>
2017-09-25 17:12:07 -0700

    app will successfully write to auth database, but it doesn't look like it's working the first time.

--- app/src/main/java/com/epicodus/dunlaprestaurants/ui/CreateAccountActivity.java
+++ app/src/main/java/com/epicodus/dunlaprestaurants/ui/CreateAccountActivity.java
@@ -1,15 +1,78 @@
 package com.epicodus.dunlaprestaurants.ui;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.epicodus.dunlaprestaurants.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener{
+    public static final String TAG = CreateAccountActivity.class.getSimpleName();
+
+    @Bind(R.id.registerButton) Button mRegisterButton;
+    @Bind(R.id.regNameEditText) EditText mRegNameEditText;
+    @Bind(R.id.regEmailEditText) EditText mRegEmailEditText;
+    @Bind(R.id.regPasswordEditText) EditText mRegPasswordEditText;
+    @Bind(R.id.regConfirmEditText) EditText mRegConfirmEditText;
+    @Bind(R.id.gotoLoginButton) Button mLoginButton;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        ButterKnife.bind(this);
+
+
+        mLoginButton.setOnClickListener(this);
+        mRegisterButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mLoginButton) {
+            Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+        if (view == mRegisterButton) {
+            createNewUser();
+        }
+    }
+
+    private void createNewUser() {
+        final String name = mRegNameEditText.getText().toString().trim();
+        final String email = mRegEmailEditText.getText().toString().trim();
+        String password = mRegPasswordEditText.getText().toString().trim();
+        String confirmPassword = mRegConfirmEditText.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Authentication successful");
+                        } else {
+                            Toast.makeText(CreateAccountActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
